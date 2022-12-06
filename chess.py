9c835410f218c2ccd845af87b8048ec854d859ec
Jodi Diffey <17diffeyj@lytchett.org.uk>
2022-12-06 20:35:32 +0000

    Load FEN into list

--- chess.py
+++ chess.py
@@ -19,12 +19,27 @@ def initialise_board(FEN=DEFAULT_FEN):
     board_position = FEN.split(" ")[0]
     white_move = True if FEN.split(" ")[1] == "w" else False
     board_position = board_position.split("/")
+    for rank_index, data in enumerate(board_position):
+        rank = 8 - rank_index
+        gap_adjustment = 0
+        for file_index, piece in enumerate(data):
+            file = file_index + 1 + gap_adjustment
+            colour = "White" if piece.lower() == piece else "Black"
+            if piece.lower() in letter_to_piece_dict:
+                pieces.append(letter_to_piece_dict[piece.lower()](rank, file, colour))
+            else:
+                gap_adjustment += int(piece) - 1
+                
+    return pieces
 
 class Piece(pygame.sprite.Sprite):
     def __init__(self, rank, file, colour):
         self.rank = rank
         self.file = file
         self.colour = colour
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} piece on rank {self.rank} on file {self.file}"
 
 class Pawn(Piece):
     def __init__(self, rank, file, colour):
@@ -51,8 +66,8 @@ class King(Piece):
         super().__init__(rank, file, colour)
 
 letter_to_piece_dict = {"p": Pawn, "r": Rook, "n": Knight, "b": Bishop, "q": Queen, "k": King}
-pieces = []
-white_move = True
+pieces = initialise_board()
+print(pieces)
 
 running = True
 while running:
