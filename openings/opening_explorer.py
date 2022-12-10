a54dea0900b8d299bc3aaa8fe071e3a4bd27ffb3
Jodi Diffey <17diffeyj@lytchett.org.uk>
2022-12-10 16:55:34 +0000

    added search box

--- openings/opening_explorer.py
+++ openings/opening_explorer.py
@@ -7,9 +7,24 @@ with open("openings/eco.json", "r") as f:
 name_FEN_index_triplets = [[opening["eco"]+": "+opening["name"], opening["fen"], i] for i, opening in enumerate(openings)]
 name_FEN_index_triplets.sort()
 
+def update_searchbox(data: list, suggested):
+    suggested.delete(0, tk.END)
+    for item in data:
+        suggested.insert(tk.END, item)
+
+def check(event, search_box, suggested):
+    text = search_box.get()
+    if text == "":
+        data = [opening[0] for opening in name_FEN_index_triplets]
+    else:
+        data = []
+        for item in [opening[0] for opening in name_FEN_index_triplets]:
+            if text.lower() in item.lower():
+                data.append(item)
+    update_searchbox(data, suggested)
+
 def return_opening(opening, window):
     global FEN_to_return
-    print(opening)
     for triplet in name_FEN_index_triplets:
         if opening == triplet[0]:
             FEN_to_return = triplet[1]
@@ -22,10 +37,21 @@ def open_window():
 
     chosen_opening = tk.StringVar(window)
     chosen_opening.set("B00: King's Pawn")
+    top_label = tk.Label(window, text="Chosen Opening:")
+    top_label.pack(pady=10)
     drop_down = tk.OptionMenu(window, chosen_opening, *[opening[0] for opening in name_FEN_index_triplets])
     drop_down.pack()
     return_button = tk.Button(text="Confirm", command=lambda: return_opening(chosen_opening.get(), window)) #idk how to return something back to the main program
-    return_button.pack()
+    return_button.pack(pady=10)
+    search_label = tk.Label(text="Search:")
+    search_label.pack()
+    search_box = tk.Entry(window, width=50)
+    search_box.pack(pady=10)
+    suggested = tk.Listbox(window, width=100)
+    suggested.pack()
+    update_searchbox([opening[0] for opening in name_FEN_index_triplets], suggested)
+    suggested.bind("<<ListboxSelect>>", lambda x: chosen_opening.set(suggested.get(tk.ACTIVE)))
+    search_box.bind("<KeyRelease>", lambda x: check(x, search_box, suggested))
     window.mainloop()
     return FEN_to_return
 
