0c3b6df717f3f67a5aa161b2e53f439eb07d68f2
Jodi Diffey <17diffeyj@lytchett.org.uk>
2022-12-11 22:14:06 +0000

    cleaner and chosen opening updates correctly

--- openings/opening_explorer.py
+++ openings/opening_explorer.py
@@ -12,9 +12,16 @@ def update_searchbox(data: list, suggested):
     for item in data:
         suggested.insert(tk.END, item)
 
+def delay_label(event, chosen_display, suggested, chosen_opening, window):
+    window.after(100, lambda: update_label(chosen_display, suggested, chosen_opening))
+
+def update_label(chosen_display, suggested, chosen_opening):
+    chosen_opening.set(suggested.get(tk.ACTIVE))
+    chosen_display["text"] = chosen_opening.get()
+
 def check(event, search_box, suggested):
     text = search_box.get()
-    if text == "":
+    if not text:
         data = [opening[0] for opening in name_FEN_index_triplets]
     else:
         data = []
@@ -39,8 +46,8 @@ def open_window():
     chosen_opening.set("B00: King's Pawn")
     top_label = tk.Label(window, text="Chosen Opening:")
     top_label.pack(pady=10)
-    drop_down = tk.OptionMenu(window, chosen_opening, *[opening[0] for opening in name_FEN_index_triplets])
-    drop_down.pack()
+    chosen_display = tk.Label(window, text=chosen_opening.get(), font=("Segoe UI",15))
+    chosen_display.pack()
     return_button = tk.Button(text="Confirm", command=lambda: return_opening(chosen_opening.get(), window)) #idk how to return something back to the main program
     return_button.pack(pady=10)
     search_label = tk.Label(text="Search:")
@@ -50,7 +57,7 @@ def open_window():
     suggested = tk.Listbox(window, width=100)
     suggested.pack()
     update_searchbox([opening[0] for opening in name_FEN_index_triplets], suggested)
-    suggested.bind("<<ListboxSelect>>", lambda x: chosen_opening.set(suggested.get(tk.ACTIVE)))
+    suggested.bind("<<ListboxSelect>>", lambda x: delay_label(x, chosen_display, suggested, chosen_opening, window))
     search_box.bind("<KeyRelease>", lambda x: check(x, search_box, suggested))
     window.mainloop()
     return FEN_to_return
