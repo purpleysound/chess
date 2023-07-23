20229577d3817e7d4304c2c1797705b8fcaad6a4
Jodi Diffey <17diffeyj@lytchett.org.uk>
2023-07-23 17:10:33 +0100

    rename and pos_to_indices

--- utils_and_constants.py
+++ utils_and_constants.py
@@ -1,8 +1,12 @@
 
-def tuple_add(t1: tuple, t2: tuple) -> tuple:
+def vector_add(t1: tuple, t2: tuple) -> tuple:
     """add two tuples together"""
     return tuple(sum(i) for i in zip(t1, t2))
 
-def indices_to_coords(index: int, jndex: int) -> tuple[int, int]:
+def indices_to_pos(index: int, jndex: int) -> tuple[int, int]:
     """converts indices to coordinates"""
     return (jndex + 1, index + 1)
+
+def pos_to_indices(coords: tuple[int, int]) -> tuple[int, int]:
+    """converts coordinates to indices"""
+    return (coords[1] - 1, coords[0] - 1)
